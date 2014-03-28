26df5e2d2255cbd00c5bed63ff32b71234b051c8
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-03-28 20:01:43 +0100

    Added leiningen default project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to webgl-cljs
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/webgl_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns webgl-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [webgl-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
