052950417ba15f5f29c3494322980ee9043e68fb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-03-28 20:18:45 +0100

    Added enough information to get to ClojureScript REPL.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2197"]
+                 [compojure "1.1.6"]
+                 [ring "1.2.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:builds {}})
