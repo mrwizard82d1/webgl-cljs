e023b85ec7cdfd4b0702eb1ac32eb4013fd3067c
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-04-01 05:42:22 +0100

    Refactored code to use dommy and cljsthree libraries.

--- src/cljs/real/webgl_cljs/real.cljs
+++ src/cljs/real/webgl_cljs/real.cljs
@@ -1,45 +1,51 @@
-(ns webgl-cljs.real)
+(ns webgl-cljs.real
+  (:require [dommy.core :as dc]
+            [cljsthree.renderers.webgl :as c3js.renderers]
+            [cljsthree.scene.scene :as c3js.scenes]
+            [cljsthree.cameras.perspective :as c3js.cameras]
+            [cljsthree.objects.mesh :as c3js.meshes]
+            )
+  (:require-macros [dommy.macros :as dm]))
 
 (defn on-mouse-up [animating event]
-  (.preventDefault event)
   (swap! animating not))
 
 (defn add-mouse-handler [renderer animating]
   (let [dom (.-domElement renderer)]
-    (.addEventListener dom "mouseup" (partial on-mouse-up animating) false)))
+    (dc/listen! dom :click (partial on-mouse-up animating))))
 
-(defn run [renderer scene camera cube animating]
-  (.render renderer scene camera)
+(defn run [render-scene cube animating]
+  (render-scene)
   
   (if @animating
     (set! (.-y (.-rotation cube)) (- (.-y (.-rotation cube)) 0.01)))
 
-  (.requestAnimationFrame js/window (partial run renderer scene camera cube animating)))
+  (.requestAnimationFrame js/window (partial run render-scene cube animating)))
 
 (defn on-load []
-  (let [container (.getElementById js/document "container")
+  (let [container (dm/sel1 :#container)
         width (.-offsetWidth container)
         height (.-offsetHeight container)
-        renderer (THREE.WebGLRenderer. (clj->js {:antialias true}))]
-    (.setSize renderer width height)
-    (.appendChild container (.-domElement renderer))
-    (let [scene (THREE.Scene.)
-          camera (THREE.PerspectiveCamera. 45 (/ width height) 1 4000)]
-      (.set (.-position camera) 0 0 3)
+        renderer (c3js.renderers/webgl {:antialias true})]
+    (c3js.renderers/set-size renderer width height)
+    (dc/append! container (.-domElement renderer))
+    (let [scene (c3js.scenes/scene)
+          camera (c3js.cameras/perspective 45 (/ width height) 1 4000)]
+      (c3js.cameras/set-position camera [0 0 3])
       (let [light (THREE.DirectionalLight. 0xffffff 1.5)]
         (.set (.-position light) 0 0 1)
-        (.add scene light)
         (let [map-url "images/mr_wizard.002.jpg"
               texture-map (. THREE.ImageUtils loadTexture map-url)
               material (THREE.MeshPhongMaterial. (clj->js {:map texture-map}))
               geometry (THREE.CubeGeometry. 1 1 1)
-              cube (THREE.Mesh. geometry material)
+              cube (c3js.meshes/mesh geometry material)
               animating (atom false)]
           (set! (.-x (.-rotation cube)) (/ Math/PI 5))
           (set! (.-y (.-rotation cube)) (/ Math/PI 5))
-          (.add scene cube)
-          (.render renderer scene camera)
-          (add-mouse-handler renderer animating)
-          (run renderer scene camera cube animating))))))
+          (c3js.scenes/add scene light cube)
+          (let [render-scene (fn [] (c3js.renderers/render renderer scene camera))]
+            (render-scene)
+            (add-mouse-handler renderer animating)
+            (run render-scene cube animating)))))))
   
 (set! (.-onload js/window) on-load)
